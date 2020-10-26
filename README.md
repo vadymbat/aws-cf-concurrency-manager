9277de4b7ff466bef5b97a62148a961ac3410e31
Vadym Batov <vadymbatov@Vadyms-MacBook-Pro.local>
2020-10-26 11:31:09 +0200

    rename macro

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # AWS CloudFormation thread manager
-![Build Status](https://travis-ci.org/vadymbat/cf_parallelizer.svg?branch=master)
+![Build Status](https://travis-ci.org/vadymbat/aws-cf-concurrency-manager.svg?branch=master)
 
 The CloudFormation doesn't manage or check resource creation limits during the deployment. The macro helps to deal with AWS API throttling during creating a lot of resources e.g. DynamoDB tables via CloudFormation. It creates different resource queues using `DependsOn` statement.
 
 ## How to deploy the macro
 1. clone the repo
 
-`git clone https://github.com/vadymbat/cf_parallelizer.git`
+`git clone https://github.com/vadymbat/aws-cf-concurrency-manager.git`
 
 2. prepare CloudFormation template
 
@@ -17,7 +17,7 @@ The CloudFormation doesn't manage or check resource creation limits during the d
 `aws cloudformation deploy --template-file cf_parrallelizer.yaml --stack-name cf-parrallelizer`
 
 ## How to use in your CloudFormation template
-1. add "Transform" statement `Transform: ParallelizerMacro`
+1. add "Transform" statement `Transform: CfConcurrencyMacro`
 2. put `ParallelTaskQuantity: <int>` to the root level of your template to set in how many threads your infrastructure will be deployed 
 3. (optional) fill `CommonDependancies: ["<ResourceLogicalId>"]` list of the resources which are dependencies of all resources in the stack
 
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import json
 import logging
-import parallelizer_macro
+import cf_concurrency_macro
 
 
 logging.basicConfig(level=logging.INFO)
@@ -8,8 +8,8 @@ SAMPLE_TEMAPLATE = "demo.json"
 
 
 def check_resource_quantity(old_fagment, new_fragment):
-    assert len(old_fagment[parallelizer_macro.CF_RESOURCES]) == len(
-        new_fragment[parallelizer_macro.CF_RESOURCES]
+    assert len(old_fagment[cf_concurrency_macro.CF_RESOURCES]) == len(
+        new_fragment[cf_concurrency_macro.CF_RESOURCES]
     )
     logging.info(
         "The resource quanity in the processed template is the same as in the original."
@@ -17,9 +17,9 @@ def check_resource_quantity(old_fagment, new_fragment):
 
 
 def check_all_resource_present(old_fagment, new_fragment):
-    for old_resource in old_fagment[parallelizer_macro.CF_RESOURCES]:
+    for old_resource in old_fagment[cf_concurrency_macro.CF_RESOURCES]:
         assert (
-            old_resource in new_fragment[parallelizer_macro.CF_RESOURCES].keys()
+            old_resource in new_fragment[cf_concurrency_macro.CF_RESOURCES].keys()
         )
     logging.info(
         "All the resource from the original tempalate are present in the new one."
@@ -29,17 +29,17 @@ def check_all_resource_present(old_fagment, new_fragment):
 if __name__ == "__main__":
     with open(SAMPLE_TEMAPLATE) as sample_template:
         event = {
-            parallelizer_macro.FRAGMENT: json.loads(sample_template.read()),
-            parallelizer_macro.REQUEST_ID: "1",
+            cf_concurrency_macro.FRAGMENT: json.loads(sample_template.read()),
+            cf_concurrency_macro.REQUEST_ID: "1",
         }
 
-    processed_event = parallelizer_macro.handler(event, "")
+    processed_event = cf_concurrency_macro.handler(event, "")
 
     check_resource_quantity(
-        event[parallelizer_macro.FRAGMENT], processed_event[parallelizer_macro.FRAGMENT]
+        event[cf_concurrency_macro.FRAGMENT], processed_event[cf_concurrency_macro.FRAGMENT]
     )
 
     check_all_resource_present(
-        event[parallelizer_macro.FRAGMENT], processed_event[parallelizer_macro.FRAGMENT]
+        event[cf_concurrency_macro.FRAGMENT], processed_event[cf_concurrency_macro.FRAGMENT]
     )
 
